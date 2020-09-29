b146fde31e2c1ead556a2ce1d6a1d2def936cfed
TobsA13 <git@tobsa.wtf>
2020-09-29 23:07:35 +0200

    Added logger class

---
+++ CASlib/Logger.py
@@ -0,0 +1,11 @@
+from logbook import StreamHandler
+import sys
+
+class Logger():
+    def __init__(self, module):
+        logbook.set_datetime_format("utc")
+        StreamHandler(sys.stdout).push_application()
+        self.logger = logbook.Logger(module)
+
+    def getLogger(self):
+        return self.logger
