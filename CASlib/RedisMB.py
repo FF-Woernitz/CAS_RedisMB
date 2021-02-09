17b8efb0a14f2eba7dabc9ad9ac9b56915c0a11a
TobsA13 <git@tobsa.wtf>
2021-02-09 15:36:40 +0100

    more universal queues

--- CASlib/RedisMB.py
+++ CASlib/RedisMB.py
@@ -2,7 +2,7 @@ import redis, os, json, uuid
 from . import Logger
 
 
-class RedisMB():
+class RedisMB:
     def __init__(self, data=None):
         self.logger = Logger.Logger(self.__class__.__name__).getLogger()
 
@@ -14,18 +14,24 @@ class RedisMB():
             host = os.getenv('REDIS_HOST', '127.0.0.1')
             port = os.getenv('REDIS_PORT', 6379)
             db = os.getenv('REDIS_DB', 0)
-        self.r = redis.Redis(host, port=port, db=db, health_check_interval=15, socket_connect_timeout=10)
+        self.r = redis.Redis(host, port=port, db=db, health_check_interval=15,
+                             socket_connect_timeout=10)
         self.p = self.r.pubsub(ignore_subscribe_messages=True)
         self.subthread = None
 
-        self.logger.info("Connecting to Redis DB on {}:{} DB: {}".format(host, port, db))
+        self.logger.info(
+            "Connecting to Redis DB on {}:{} DB: {}".format(host, port, db))
         self.r.ping()
-        self.logger.info("Connected successfully to Redis DB on {}:{} DB: {}".format(host, port, db))
+        self.logger.info(
+            "Connected successfully to Redis DB on {}:{} DB: {}".format(host,
+                                                                        port,
+                                                                        db))
 
     def _publish_message(self, queue, message):
         message['uuid'] = str(uuid.uuid1())
         message['type'] = queue
-        message = json.dumps(message, separators=(',', ':'), sort_keys=True, indent=None)
+        message = json.dumps(message, separators=(',', ':'), sort_keys=True,
+                             indent=None)
         self.logger.trace("Sending redis message: {}".format(message))
         self.r.publish(queue, message)
 
@@ -35,21 +41,17 @@ class RedisMB():
     def exit(self):
         self.r.close()
 
-    def _newZVEI(self, zvei, type):
-        message = {"zvei": zvei}
-        self._publish_message(type, message)
+    def input(self, messagetype, data):
+        self._publish_message("input", {"type": messagetype, "data": data})
 
-    def inputZVEI(self, zvei):
-        self._newZVEI(zvei, "inputZVEI")
+    def alert(self, messagetype, data):
+        self._publish_message("alert", {"type": messagetype, "data": data})
 
-    def alertZVEI(self, zvei):
-        self._newZVEI(zvei, "alertZVEI")
+    def error(self, messagetype, data):
+        self._publish_message("error", {"type": messagetype, "data": data})
 
-    def errorZVEI(self, zvei):
-        self._newZVEI(zvei, "errorZVEI")
-
-    def testalertZVEI(self, zvei):
-        self._newZVEI(zvei, "testalertZVEI")
+    def test(self, messagetype, data):
+        self._publish_message("test", {"type": messagetype, "data": data})
 
     def subscribeToType(self, type, callback):
         self.p.subscribe(**{type: callback})
