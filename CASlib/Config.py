d82a4e2bb2af63c7b9df0ab42686a5ecdcd0270a
TobsA13 <git@tobsa.wtf>
2021-02-09 15:37:18 +0100

    align Config.py to new config layout

--- CASlib/Config.py
+++ CASlib/Config.py
@@ -13,7 +13,20 @@ class Config():
                 except Exception as e:
                     raise Exception("Failed to parse config file. {}".format(e))
                 if len(config["trigger"]) == 0:
-                    raise Exception("Please check config file. No trigger defined")
+                    raise Exception("No trigger defined! Please check config file.")
+                triggerCount = 0
+                for triggerGroup in config["trigger"]:
+                    triggerCount += len(triggerGroup)
+                if triggerCount == 0:
+                    raise Exception("No trigger defined! Please check config file.")
+                if len(config["action"]) == 0:
+                    self.logger.warn("You have no actions defined. Please check config file.")
+                for triggerGroup in config["trigger"]:
+                    for trigger in triggerGroup:
+                        if not "action" in trigger or len(trigger["action"]):
+                            self.logger.warn(
+                                "You have no actions defined in trigger {}. Please check config file.".format(trigger["name"]))
+
                 self.config = config
         except OSError:
             raise Exception("Can't find config file please check mount point")
