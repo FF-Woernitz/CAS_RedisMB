ff62ff0e5c83eb1c19797eb184da266df9ece31c
TobsA13 <git@tobsa.wtf>
2021-02-10 16:26:17 +0100

    Rename lib

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ setuptools.setup(
     author="FF Woernitz",
     author_email="technik@ff-woernitz.de",
     description="The universal lib used in the CAS system",
-    url="https://github.com/FF-Woernitz/CAS_lib",
+    url="https://github.com/FF-Woernitz/CAS_library",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3"
