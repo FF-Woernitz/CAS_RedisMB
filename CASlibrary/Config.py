021bda4ad1ee510b38222c48557108cba18533ce
TobsA13 <git@tobsa.wtf>
2021-03-16 21:20:26 +0100

    other config format
    Signed-off-by: TobsA13 <git@tobsa.wtf>

--- CASlibrary/Config.py
+++ CASlibrary/Config.py
@@ -3,7 +3,7 @@ import json
 from . import Logger
 
 
-class Config():
+class Config:
     def __init__(self):
         self.logger = Logger.Logger(self.__class__.__name__).getLogger()
         try:
@@ -13,32 +13,28 @@ class Config():
                 except Exception as e:
                     raise Exception("Failed to parse config file. {}".format(e))
 
-                if len(config["trigger"]) == 0:
-                    self.logger.warn("No trigger defined! Please check config file.")
+                if "trigger" not in config or "action" not in config:
+                    raise Exception("Failed to parse config file. Missing required trigger or action")
 
-                triggerCount = 0
-                for triggerGroup in config["trigger"].values():
-                    triggerCount += len(triggerGroup)
-                if triggerCount == 0:
+                if len(config["trigger"]) == 0:
                     self.logger.warn("No trigger defined! Please check config file.")
 
                 if len(config["action"]) == 0:
                     self.logger.warn("You have no actions defined. Please check config file.")
 
-                for triggerGroup in config["trigger"].values():
-                    for trigger in triggerGroup.values():
-                        if "action" not in trigger or len(trigger["action"]) <= 0:
-                            self.logger.warn("You have no actions defined in trigger {}."
-                                             " Please check config file.".format(trigger["name"]))
-                        else:
-                            for action in trigger["action"]:
-                                if action not in config["action"]:
-                                    self.logger.warn("Action {} in trigger {} is not defined."
-                                                     " Please check config file.".format(action, trigger["name"]))
+                for trigger in config["trigger"].values():
+                    if "action" not in trigger or len(trigger["action"]) <= 0:
+                        self.logger.warn("You have no actions defined in trigger {}."
+                                         " Please check config file.".format(trigger["name"]))
+                    else:
+                        for action in trigger["action"]:
+                            if action not in config["action"]:
+                                self.logger.warn("Action {} in trigger {} is not defined."
+                                                 " Please check config file.".format(action, trigger["name"]))
 
                 self.config = config
         except OSError:
-            raise Exception("Can't find config file please check mount point")
+            raise Exception("Can't open config file. Please check mount point")
         self.logger.info("Successfully loaded config.json")
 
     def getConfig(self):
