1110eebbe64edd425623e5cdddb7bbd57da94369
TobsA13 <git@tobsa.wtf>
2021-04-13 15:56:11 +0200

    Fixed breaking bug Add env option for the config files location Add more logging
    Signed-off-by: TobsA13 <git@tobsa.wtf>

--- CASlibrary/Config.py
+++ CASlibrary/Config.py
@@ -1,13 +1,15 @@
+import os.path
+import time
 import json
 
 from . import Logger
 
-
 class Config:
     def __init__(self):
         self.logger = Logger.Logger(self.__class__.__name__).getLogger()
+        config_file_location = os.getenv('CAS_CONFIG', '/opt/config.json')
         try:
-            with open("/opt/config.json") as config_file:
+            with open(config_file_location) as config_file:
                 try:
                     config = json.load(config_file)
                 except Exception as e:
@@ -22,7 +24,7 @@ class Config:
                 if len(config["action"]) == 0:
                     self.logger.warn("You have no actions defined. Please check config file.")
 
-                for trigger in config["trigger"].values():
+                for trigger in config["trigger"]:
                     if "action" not in trigger or len(trigger["action"]) <= 0:
                         self.logger.warn("You have no actions defined in trigger {}."
                                          " Please check config file.".format(trigger["name"]))
@@ -36,6 +38,11 @@ class Config:
         except OSError:
             raise Exception("Can't open config file. Please check mount point")
         self.logger.info("Successfully loaded config.json")
+        self.logger.info("Config file last modified: {}".format(time.ctime(os.path.getmtime(config_file_location))))
+        if "version" in self.config:
+            self.logger.info("Config file version: {}".format(self.config["version"]))
+        else:
+            self.logger.info("Config file version: No version key found")
 
     def getConfig(self):
         return self.config
